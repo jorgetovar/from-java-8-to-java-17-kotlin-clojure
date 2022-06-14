a8f275c7143ad85899d46571889f7e116755c7fc
jorgetovar <jorgetovar621@gmail.com>
2022-06-14 12:39:09 -0500

    Initial commit

--- java8/src/test/java/com/simplify/java8/Java8ApplicationTests.java
+++ java8/src/test/java/com/simplify/java8/Java8ApplicationTests.java
@@ -1,10 +1,10 @@
-package com.simplify.java17;
+package com.simplify.java8;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Java17ApplicationTests {
+class Java8ApplicationTests {
 
 	@Test
 	void contextLoads() {
