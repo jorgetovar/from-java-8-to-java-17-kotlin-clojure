758b53b91763a1b34883ec9b97c29ee55a436dc2
Jorge Tovar <jtovar@atlassian.com>
2022-07-02 10:57:45 -0500

    Testing files TDD

--- ktlin/src/test/kotlin/com/simplify/ktlin/KtlinApplicationTests.kt
+++ ktlin/src/test/kotlin/com/simplify/ktlin/KtlinApplicationTests.kt
@@ -1,13 +1,39 @@
 package com.simplify.ktlin
 
+import com.simplify.ktlin.book.BookParser
+import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
-import org.springframework.boot.test.context.SpringBootTest
+import java.nio.file.Paths
 
-@SpringBootTest
 class KtlinApplicationTests {
 
-	@Test
-	fun contextLoads() {
-	}
+    @Test
+    fun testConvertCsvToJson_WhenIsAProgrammingBook() {
+        val expected = """
+			[{"title":"Code Complete",
+			   "author":"Steve McConnell",
+			   "pages":960,
+			   "karma":40,
+			   "eBook":true,
+			   "category":"programming"}]
+                """.trimIndent().replace("\n", "")
+        val currentRelativePath = Paths.get("src/test/resources")
+        val actual: String = BookParser().convertCsvToJson("$currentRelativePath/programming-test.csv", "$currentRelativePath/programming-test.json")
+        Assertions.assertEquals(expected, actual)
+    }
+
+    @Test
+    fun testConvertCsvToJson_WhenIsAFictionBook() {
+        val expected = """
+		  [{"title":"Book Lovers",
+            "author":"Emily Henry",
+            "pages":1220,"karma":25,
+            "eBook":false,
+            "category":"fiction"}]
+            """.trimIndent().replace("\n", "")
+        val currentRelativePath = Paths.get("src/test/resources")
+        val actual: String = BookParser().convertCsvToJson("$currentRelativePath/fiction-test.csv", "$currentRelativePath/fiction-test.json")
+        Assertions.assertEquals(expected, actual)
+    }
 
 }
