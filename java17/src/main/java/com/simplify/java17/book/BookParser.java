81bca62bb20f78225417fc7ad2ffbee9d865208d
Jorge Tovar <jtovar@atlassian.com>
2022-07-06 17:05:01 -0500

    Initial commit

--- java17/src/main/java/com/simplify/java17/book/BookParser.java
+++ java17/src/main/java/com/simplify/java17/book/BookParser.java
@@ -21,48 +21,51 @@ public class BookParser {
 
     public String convertCsvToJson(String inputCsv, String outputJson) {
 
-        String csvLine, json = "[";
+        String csvLine;
+        var json = "[";
 
 
-        try (BufferedReader csvReader = new BufferedReader(new FileReader(inputCsv));
-             BufferedWriter jsonWriter = new BufferedWriter(new FileWriter(outputJson))) {
+        try (var csvReader = new BufferedReader(new FileReader(inputCsv));
+             var jsonWriter = new BufferedWriter(new FileWriter(outputJson))) {
 
             String[] csvFields;
 
             while ((csvLine = csvReader.readLine()) != null) {
                 csvFields = csvLine.split(",");
 
-                String title = csvFields[CsvColumns.TITLE.ordinal()];
-                String author = csvFields[CsvColumns.AUTHOR.ordinal()];
-                int pages = Integer.parseInt(csvFields[CsvColumns.PAGES.ordinal()]);
+                var title = csvFields[CsvColumns.TITLE.ordinal()];
+                var author = csvFields[CsvColumns.AUTHOR.ordinal()];
+                var pages = Integer.parseInt(csvFields[CsvColumns.PAGES.ordinal()]);
                 Category category = Category.valueOf(csvFields[CsvColumns.CATEGORY.ordinal()]);
-                int karma = 0;
+                var karma = 0;
                 Book book = null;
                 switch (category) {
-                    case fiction:
+                    case fiction -> {
                         karma = 25;
                         book = new FictionBook(title, author);
-                        break;
-                    case programming:
+                    }
+                    case programming -> {
                         karma = 40;
                         book = new ProgrammingBook(title, author);
-                        break;
-                    case psychological:
+                    }
+                    case psychological -> {
                         karma = 30;
                         book = new PsychologicalBook(title, author);
-                        break;
+                    }
                 }
-                json += "{";
-                json += "\"title\":\"" + title + "\",";
-                json += "\"author\":\"" + author + "\",";
-                json += "\"pages\":" + pages + ",";
-                json += "\"karma\":" + karma + ",";
-                json += "\"eBook\":" + BookInOReally.available(book) + ",";
-                json += "\"category\":\"" + category + "\"";
-                json += "},";
+
+                json += """
+                        {"title":"%s",
+                        "author":"%s",
+                        "pages":%s,
+                        "karma":%s,
+                        "eBook":%s,
+                        "category":"%s"},""".formatted(title, author, pages, karma, BookInOReally.available(book), category);
+
+
             }
-            json = json.substring(0, json.length() - 1);
-            json += "]";
+            json = json.substring(0, json.length() - 1) + "]";
+
 
             jsonWriter.write(json);
 
@@ -70,7 +73,7 @@ public class BookParser {
             e.printStackTrace();
         }
 
-        return json;
+        return json.replace("\n", "");
     }
 
 }
