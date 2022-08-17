7d7fab25508f4558f459e77067c1c0b4c23f6d30
Jorge Tovar <jtovar@atlassian.com>
2022-08-17 16:59:28 -0500

    Use io/resource to improve file-path using absolute URL

--- clj-11/src/clj_11/core.clj
+++ clj-11/src/clj_11/core.clj
@@ -1,8 +1,10 @@
 (ns clj-11.core
-  (:require [clj-11.book :as book])
+  (:require [clj-11.book :as book]
+            [clojure.java.io :as io]
+            )
   (:gen-class))
 
-(def file-path "/Users/jtovar/Documents/personal-spikes/from-java-8-to-java-17-kotlin-clojure/clj-11/src/resources/programming-test.csv")
+(def file-path (io/resource "resources/programming-test.csv"))
 
 
 (defn -main
