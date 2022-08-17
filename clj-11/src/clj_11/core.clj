c195f11fbfaed7343dbd53c649569aabbdfc0d27
Jorge Tovar <jtovar@atlassian.com>
2022-08-17 16:48:08 -0500

    Clojure initial commit

--- clj-11/src/clj_11/core.clj
+++ clj-11/src/clj_11/core.clj
@@ -1,8 +1,12 @@
-
 (ns clj-11.core
+  (:require [clj-11.book :as book])
   (:gen-class))
 
+(def file-path "/Users/jtovar/Documents/personal-spikes/from-java-8-to-java-17-kotlin-clojure/clj-11/src/resources/programming-test.csv")
+
+
 (defn -main
-  "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (println (book/csv->json file-path)))
+
+
