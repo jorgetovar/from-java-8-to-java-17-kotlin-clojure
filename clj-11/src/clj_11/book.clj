dcff48290fe3c6cbf844b0efae56c8f0a7101560
jorgetovar <jorgetovar621@gmail.com>
2024-04-15 12:39:54 -0500

    destructuring

--- clj-11/src/clj_11/book.clj
+++ clj-11/src/clj_11/book.clj
@@ -1,68 +1,66 @@
 (ns clj-11.book
-  (:require [clojure.string :as str])
-  (:import java.math.RoundingMode)
-  )
+    (:require [clojure.string :as str])
+    (:import java.math.RoundingMode)
+    )
 
 (defn- read-csv [path]
-  "Read file as string and remove breaking lines"
-  (str/replace (slurp path) #"\n" ""))
+       "Read file as string and remove breaking lines"
+       (str/replace (slurp path) #"\n" ""))
 
 (defn- karma-by-category [category]
-  "Get the karma based on book category"
-  (category {:fiction       25
-             :programming   40
-             :psychological 30}))
+       "Get the karma based on book category"
+       (category {:fiction       25
+                  :programming   40
+                  :psychological 30}))
 
 (defn- is-ebook? [category]
-  "Get the kind of book based on book category"
-  (category {:fiction       false
-             :programming   true
-             :psychological false}))
+       "Get the kind of book based on book category"
+       (category {:fiction       false
+                  :programming   true
+                  :psychological false}))
 
 (defn- is-number? [element]
-  "Try to cast the element to a bigDecimal to identify the element as a valid number"
-  (try
-    (number? (bigdec element))
-    (catch Exception _
-      false))
-  )
+       "Try to cast the element to a bigDecimal to identify the element as a valid number"
+       (try
+         (number? (bigdec element))
+         (catch Exception _
+           false))
+       )
 
 (defn- average [coll]
-  "Get all the valid rates and cast to bigDecimal"
-  (let [sum (reduce + coll)
-        count (count coll)]
-    (.divide sum (bigdec count) 2 RoundingMode/HALF_UP))
-  )
+       "Get all the valid rates and cast to bigDecimal"
+       (let [sum (reduce + coll)
+             count (count coll)]
+            (.divide sum (bigdec count) 2 RoundingMode/HALF_UP))
+       )
 
 (defn- rate [content-list]
-  "Get the average rate from numbers raging between 1 and 5 "
-  (let [all-numbers (map #(bigdec %) (filter is-number? content-list))
-        valid-rates (filter #(<= 1 % 5) all-numbers)]
-    (average valid-rates)
-    ))
+       "Get the average rate from numbers raging between 1 and 5 "
+       (let [all-numbers (map #(bigdec %) (filter is-number? content-list))
+             valid-rates (filter #(<= 1 % 5) all-numbers)]
+            (average valid-rates)
+            ))
 
 (defn- create-json-record [content-list]
-  "map to a edn using the list index"
-  (let [title (nth content-list 0)
-        author (nth content-list 1)
-        category (keyword (nth content-list 3))
-        pages (Integer/parseInt (nth content-list 2))
-        karma (karma-by-category category)
-        rate (rate content-list)
-        ebook (is-ebook? category)]
-    {:title    title
-     :author   author
-     :pages    pages
-     :category category
-     :karma    karma
-     :rate     rate
-     :ebook    ebook
-     }))
+       "map to a edn using the list index"
+       (let [[title author pages c] content-list
+             category (keyword c)
+             karma (karma-by-category category)
+             rate (rate content-list)
+             ebook (is-ebook? category)]
+            {:title    title
+             :author   author
+             :pages    (Integer/parseInt pages)
+             :category category
+             :karma    karma
+             :rate     rate
+             :ebook    ebook
+             }))
 
 (defn csv->json [file-path]
-  "Read the document and convert it to a valid EDN"
-  (let [csv-content (read-csv file-path)
-        content-list (str/split csv-content #",")
-        content (create-json-record content-list)]
-    content))
+      "Read the document and convert it to a valid EDN"
+      (let [csv-content (read-csv file-path)
+            content-list (str/split csv-content #",")
+            content (create-json-record content-list)]
+           content))
 
